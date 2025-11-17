b53f4cf61767881b2b0e9da12f6fa426f4193ff9
Laxman kendre <106816249+lkendre5652@users.noreply.github.com>
2025-11-17 19:08:24 +0530

    Fix: Added STATIC_ROOT for collectstatic

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -121,3 +121,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+
+# Required for CI/CD and production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# If you already have STATICFILES_DIRS, keep them:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
