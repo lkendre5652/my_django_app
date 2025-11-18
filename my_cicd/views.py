e031dc29b5b51d6a7bf074ad5c0996235d043146
Laxman kendre <106816249+lkendre5652@users.noreply.github.com>
2025-11-18 18:12:10 +0530

    authetications added

--- my_cicd/views.py
+++ my_cicd/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from rest_framework.generics import ListAPIView, CreateAPIView
 from .models import Blog, BlogCategory
 from .serializer import BlogCategorySerial, BlogSerial
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class BlogListAPI(ListAPIView):
     queryset = BlogCategory.objects.filter(status=1)
@@ -11,6 +14,8 @@ class BlogListAPI(ListAPIView):
 class BlogCategoryAPI(ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerial
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 # Create your views here.
 def get_hello(request):
